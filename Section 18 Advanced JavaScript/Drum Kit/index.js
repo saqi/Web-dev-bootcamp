c093b2fc7725b45784fd7c8391487ff710f37257
Saqlain <saqisaq@hotmail.com>
2025-11-21 12:45:22 +0000

    Function to play sound and play sound on keydown (key press)

--- Section 18 Advanced JavaScript/Drum Kit/index.js
+++ Section 18 Advanced JavaScript/Drum Kit/index.js
@@ -1,3 +1,5 @@
+//code to play sound based on button clicked
+
 var numberOfDrums = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrums; i++) {
@@ -6,44 +8,57 @@ for (var i = 0; i < numberOfDrums; i++) {
 
     var buttonText = this.innerHTML;
 
-    switch (buttonText) {
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "s":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case "j":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "k":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      case "l":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      default:
-        console.log(buttonText);
-    } // end of switch
+    playSound(buttonText);
   }); // end of anonymous function
 } // end of for loop
+
+//function to play sound based on letter
+
+function playSound(letter) {
+  switch (letter) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "k":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "l":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    default:
+      console.log(buttonText);
+  } // end of switch
+} // end of playSound function
+
+//code to use playSound function on keydown (key press)
+
+document.addEventListener("keydown", function (event) {
+  var keyPressed = event.key;
+  playSound(keyPressed);
+}); // end of event listener for key press
