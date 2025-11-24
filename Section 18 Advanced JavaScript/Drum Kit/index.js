95e3222c16b9d7f19bb0b8c6aded341a84217802
Saqlain <saqisaq@hotmail.com>
2025-11-24 17:24:10 +0000

    Style button when clicked or letter pressed

--- Section 18 Advanced JavaScript/Drum Kit/index.js
+++ Section 18 Advanced JavaScript/Drum Kit/index.js
@@ -9,11 +9,11 @@ for (var i = 0; i < numberOfDrums; i++) {
     var buttonText = this.innerHTML;
 
     playSound(buttonText);
+    animateButton(buttonText);
   }); // end of anonymous function
 } // end of for loop
 
 //function to play sound based on letter
-
 function playSound(letter) {
   switch (letter) {
     case "w":
@@ -56,9 +56,19 @@ function playSound(letter) {
   } // end of switch
 } // end of playSound function
 
+function animateButton(letter) {
+  var button = document.querySelector("." + letter);
+  button.classList.add("pressed");
+  setTimeout(function () {
+    button.classList.remove("pressed");
+  }, 150);
+}
+
 //code to use playSound function on keydown (key press)
 
 document.addEventListener("keydown", function (event) {
   var keyPressed = event.key;
   playSound(keyPressed);
+  animateButton(keyPressed);
+  //function to play sound based on letter
 }); // end of event listener for key press
